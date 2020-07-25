bdf69efdefc313537b2630a4e906e42b797d4c12
zjr1234 <280>
2020-07-25 06:40:21 +0000

    commit

--- pythontest.py
+++ pythontest.py
@@ -4,7 +4,11 @@ import sys  #引入模块
 rslt=[]
 rslt = sys.argv[0]
 print (rslt[1])
-output_string=rslt[1]
+output_string1=rslt[1][0][0]
+output_string2=rslt[1][1][0]
 
-def fun(list):
-    return output_string
+def fun(output_string1,output_string2):
+    list=[]
+    list.append(output_string1)
+    list.append(output_string2)
+    return list
