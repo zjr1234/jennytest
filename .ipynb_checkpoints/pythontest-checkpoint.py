2c14feb3559ec14bcde457c9bcbba7dbe8bea9e0
zjr1234 <280>
2020-07-25 07:23:09 +0000

    commit

--- .ipynb_checkpoints/pythontest-checkpoint.py
+++ .ipynb_checkpoints/pythontest-checkpoint.py
@@ -8,8 +8,8 @@ output_string1=rslt[0][0]
 output_string2=rslt[1][0]
 
 def fun(output_string1,output_string2):
-    list=[]
-    list.append(output_string1)
-    list.append(output_string2)
-    return list
+    listrlst=[]
+    listrlst.append(output_string1)
+    listrlst.append(output_string2)
+    return listrlst
 
